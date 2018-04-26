e08782a01b7a09337dc8e1ffdb202ac4e15ef5eb
aubelelautaro <36264982+aubelelautaro@users.noreply.github.com>
2018-04-26 12:18:04 -0300

    clase 9

---
+++ clase 9 previa parcial/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define T 3
+void mostarAlumnos()
+{
+    int i;
+    for(i=0; i<T; i++)
+    {
+        eAlumno[i].legajo=legajo[i];
+        eAlumno[i].nombre=nombre[i];
+    }
+}
+typedef struct
+{
+    char calle[50];
+    int numero;
+    int cp;
+}eDomicilio;
+
+typedef struct
+{
+    int legajo;
+    char nombre [50];
+    eDomicilio domicilio;
+
+}eAlumno;
+
+int main()
+{
+    eAlumno miAlumno;
+
+    int i;
+
+    for (i=0; i<T; i++)
+    {
+        printf("Nombre:",miAlumno.nombre);
+        fflush(stdin);
+        gets(miAlumno.nombre);
+        printf("Numero:", miAlumno.legajo);
+        scanf ("%d", &miAlumno.legajo);
+    }
+
+    mostrarAlumnos(eAlumno.miAlumno);
+    return 0;
+}
